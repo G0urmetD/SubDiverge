eea87c2ddbc6053da4dfcd7bdccf8a8c16e884fb
G0urmet <60777262+G0urmetD@users.noreply.github.com>
2024-05-08 13:18:03 +0200

    Update subDiverge.py

--- subDiverge.py
+++ subDiverge.py
@@ -3,8 +3,8 @@ import argparse
 
 from colorama import Fore, Style
 
-from modules.subfinder import check_subfinder, run_subfinder
-from modules.assetfinder import check_assetfinder, run_assetfinder
+from modules.subfinder import run_subfinder
+from modules.assetfinder import run_assetfinder
 from modules.merge_subs import merge_subdomains
 from modules.http_probe import probe_subdomains
 
@@ -29,7 +29,7 @@ def main():
     """
     print(Fore.CYAN + banner + Style.RESET_ALL)
     print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Author: G0urmetD (G0urmet)")
-    print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Version: 3.6")
+    print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Version: 3.6.4")
 
     # script arguments/parameters
     parser = argparse.ArgumentParser(description='Scan for subdomains and probe HTTP status codes.')
@@ -43,22 +43,8 @@ def main():
 
     # start tool timer
     start_time = time.time()
-    
-    # call subfinder install check function
-    print(f"=============================== {Fore.CYAN}[CHECK]{Style.RESET_ALL} ===============================")
-    # Check and install Subfinder
-    print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Checking subfinder on the system")
-    check_subfinder()
-
-    # Check and install Assetfinder
-    print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} Checking assetfinder on the system")
-    check_assetfinder()
-
-    print(f"=============================== {Fore.CYAN}[END]{Style.RESET_ALL} ===============================")
 
     # starting subdomain enumeration
-    print(f"=============================== {Fore.CYAN}[SUBDOMAINS]{Style.RESET_ALL} ===============================")
-    print("\n")
     print(f"{Fore.CYAN}[INFO]{Style.RESET_ALL} Scanning for subdomains...")
     time.sleep(2)
 
@@ -71,19 +57,16 @@ def main():
     # compare subdomain findings
     # save compared subdomains in final .txt file
     merge_subdomains("subs-subfinder.txt", "subs-assetfinder.txt", "subs-final.txt")
-
-    print(f"=============================== {Fore.CYAN}[END]{Style.RESET_ALL} ===============================")
     
     if probe_enabled:
-        print(f"=============================== {Fore.CYAN}[HTTP/HTTPS PROBE]{Style.RESET_ALL} ===============================")
         # loop through compared subdomain findings and run http/https probe & print out
         # save accessible subdomains in .txt file
         all_subdomains = read_subdomains_from_file("subs-final.txt")
-        print(f"{Fore.YELLOW}[INFO]{Style.RESET_ALL} I'm working on it, grab a coffee ...")
+        print(f"{Fore.CYAN}[INFO]{Style.RESET_ALL} Start HTTP probe - takes a bit - grab a coffee...")
         table_http, table_https = probe_subdomains(all_subdomains, timeout=timeout)
-        print(f"{Fore.MAGENTA}HTTP Probe:{Style.RESET_ALL}")
+        print(f"=========={Fore.MAGENTA} HTTP Probe{Style.RESET_ALL} ==========")
         print(table_http)
-        print(f"\n{Fore.MAGENTA}HTTPS Probe:{Style.RESET_ALL}")
+        print(f"\n========== {Fore.MAGENTA}HTTPS Probe{Style.RESET_ALL} ==========")
         print(table_https)
 
     # end tool timer & print out
